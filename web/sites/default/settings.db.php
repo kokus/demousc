c02fc74ac0ad4e08d0e41ad66f76f5b1892f6851
Andrei Khalipau <a.khalipau@gmail.com>
2023-10-17 15:16:52 -0400

    USCGOV-77: Added a secondary database.

--- web/sites/default/settings.db.php
+++ web/sites/default/settings.db.php
@@ -13,3 +13,16 @@ $databases['default']['default'] = [
   'driver' => 'mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 ];
+
+$databases['migration']['default'] = [
+  'database' => getenv('MIGRATION_DB_NAME'),
+  'username' => getenv('MIGRATION_DB_USER'),
+  'password' => getenv('MIGRATION_DB_PASS'),
+  'prefix' => '',
+  'host' => getenv('MIGRATION_DB_HOST'),
+  'port' => getenv('MIGRATION_DB_PORT'),
+  'isolation_level' => 'READ COMMITTED',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+];
