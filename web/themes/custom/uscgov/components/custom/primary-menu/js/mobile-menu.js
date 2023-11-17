8b44adc9332e8ba69fe18856d69bda59bdfda900
Mike Herchel <mike@herchel.com>
2023-11-17 16:09:48 -0500

    WIP mobile menu interactions.

--- web/themes/custom/uscgov/components/custom/primary-menu/js/mobile-menu.js
+++ web/themes/custom/uscgov/components/custom/primary-menu/js/mobile-menu.js
@@ -5,16 +5,42 @@
 ((Drupal, once) => {
   function init(menu) {
     const menuButtons = menu.querySelectorAll('.primary-menu__button');
+    const backButtons = menu.querySelectorAll('.primary-menu__mobile-back-button');
 
     menuButtons.forEach(menuButton => {
       menuButton.addEventListener('click', () => {
-        const menuButtonExpanded = menuButton.getAttribute('aria-expanded') === 'true';
-        menuButton.setAttribute('aria-expanded', !menuButtonExpanded);
-      })
+        if (Drupal.uscgov.isMobileMenuSystem()) {
+          expandMenu(menuButton);
+        }
+      });
     });
 
+    backButtons.forEach(backButton => {
+      backButton.addEventListener('click', () => {
+        if (Drupal.uscgov.isMobileMenuSystem()) {
+          navigateToParent(backButton);
+        }
+      });
+    });
+
+
     // @todo Debug code.
-    document.querySelector('[aria-controls="id-about-federal-courts-dropdown"]').click();
+    // document.querySelector('[aria-controls="id-about-federal-courts-dropdown"]').click();
+  }
+
+  function expandMenu(button) {
+    button.setAttribute('aria-expanded', true);
+  }
+
+  function navigateToParent(backButton) {
+    const closestMenuItem = backButton.closest('.primary-menu__menu-item');
+    const parentMenuButton = closestMenuItem.querySelector('.primary-menu__button');
+
+    parentMenuButton.setAttribute('aria-expanded', false);
+  }
+
+  function getMenuParentTitle() {
+
   }
 
 
