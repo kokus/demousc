7214b906fc3dddaf96e1a06e874abb1456a2008b
Mike Herchel <mike@herchel.com>
2023-11-17 14:19:14 -0500

    WIP Mobile menu interactions.

---
+++ web/themes/custom/uscgov/components/custom/primary-menu/js/mobile-menu.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Mobile menu interactions
+ */
+((Drupal, once) => {
+  function init(menu) {
+    const menuButtons = menu.querySelectorAll('.primary-menu__button');
+
+    menuButtons.forEach(menuButton => {
+      menuButton.addEventListener('click', () => {
+        const menuButtonExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+        menuButton.setAttribute('aria-expanded', !menuButtonExpanded);
+      })
+    });
+  }
+
+
+  Drupal.behaviors.mobileMenu = {
+    attach(context) {
+      once('mobile-menu', '.primary-menu--level-0', context).forEach(init);
+    },
+  };
+})(Drupal, once);
